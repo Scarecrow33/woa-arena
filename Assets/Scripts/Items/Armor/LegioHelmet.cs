3af29c87e384307cdb5a82c73025f28a1473a1ea
Wachiwi <speichermail2@aol.com>
2015-05-26 16:22:46 +0200

    Added source code for some future items
    Added source code for some future items:
    * Centurio helmet
    * Helmet
    * Sandals
    * Pilum

---
+++ Assets/Scripts/Items/Armor/LegioHelmet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helmet : Armor {
+
+	public new void Awake() {
+		base.Awake();
+		this.armorValue = 6;
+		this.CanonicalName = "Legio Helmet";
+		
+		this.slot = ArmorType.Head;
+	}
+}
