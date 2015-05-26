93519a070fecde1f7c93e9fb0c39a9d81ee84155
Wachiwi <speichermail2@aol.com>
2015-05-26 16:22:46 +0200

    Added source code for some future items
    Added source code for some future items:
    * Centurio helmet
    * Helmet
    * Sandals
    * Pilum

---
+++ Assets/Scripts/Items/Armor/CenturioHelmet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenturioHelmet : Armor {
+
+	public new void Awake() {
+		base.Awake();
+		this.armorValue = 10;
+		this.CanonicalName = "Centurio Helmet";
+		
+		this.slot = ArmorType.Head;
+	}
+}
---
+++ Assets/Scripts/Items/Armor/Sandals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sandals : Armor {
+
+	public new void Awake() {
+		base.Awake();
+		this.armorValue = 10;
+		this.CanonicalName = "Sandals";
+		
+		this.slot = ArmorType.Feet;
+	}
+}
